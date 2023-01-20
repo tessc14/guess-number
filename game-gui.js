7cd2f2b5c52fa3ab191e8719579a820e3e1af9ff
tessc14 <tessc14@gmail.com>
2023-01-20 13:02:41 +0000

    tidy up

--- game-gui.js
+++ game-gui.js
@@ -1,44 +1,27 @@
-// const prompt = require('prompt-sync')();
-// const colour = require("ansi-colors");
 
 const displayMessage = document.getElementById('displayMessage');
 const enterNumber = document.getElementById('userInput');
 const form = document.getElementById('enterNumber');
-let genNumber, userInput;
 
+let genNumber, userInput;
   
-  form.addEventListener('submit', playGame);
+form.addEventListener('submit', playGame);
 
 function playGame(event) {
     event.preventDefault();
     // generate random number between 1-10
     genNumber = Math.floor(Math.random()* 10) + 1;
     
-    // ask for user input
     userInput = enterNumber.value;
     enterNumber.value = "";
     userInput = parseInt(userInput);
-    //console.log(userInput)
 
     if (userInput <=10 && userInput >= 1) {
         checkNumber(userInput, genNumber);
     } else {
         displayMessage.textContent = "Thank you for playing!";
-    }
-    
+    } 
 }
-
-// document.querySelector('form').addEventListener("submit", playGame(e))
-
-// function playAgain() {
-//     const replay = prompt('Type x to play again or any other key to exit... ')
-
-//     if (replay === "x") {
-//         playGame()
-//     } else {
-//         console.log("Thank you for playing!")
-//     }
-// }
 
 function checkNumber(userInput, genNumber) {
     if (userInput === genNumber) {
